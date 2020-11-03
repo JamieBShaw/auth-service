55ab82f63d277eecbbab39f1b33fafb5594beec6
Jamie <jamiebshaw1095@gmail.com>
2020-11-03 20:45:13 +0000

    added auth middleware to extract token from http requests that can be used by other pkgs

---
+++ middleware/http_middleware/authentication.go
@@ -0,0 +1,92 @@
+package http_middleware
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/dgrijalva/jwt-go"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type AccessDetails struct {
+	AccessUuid string
+	UserId   int64
+}
+func AuthenticationMiddlware(next http.HandlerFunc) http.HandlerFunc {
+
+	return func(rw http.ResponseWriter, r *http.Request) {
+
+		tokenAuth, err := extractTokenMetadata(r)
+		if err != nil {
+			return
+		}
+
+		err = json.NewEncoder(rw).Encode(&tokenAuth)
+		if err != nil {
+			return
+		}
+		rw.WriteHeader(http.StatusOK)
+
+		next(rw, r)
+	}
+}
+func extractTokenMetadata(r *http.Request) (*AccessDetails, error) {
+	token, err := verifyToken(r)
+	if err != nil {
+		return nil, err
+	}
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if ok && token.Valid {
+		accessUuid, ok := claims["access_uuid"].(string)
+		if !ok {
+			return nil, err
+		}
+		userId, err := strconv.ParseInt(fmt.Sprintf("%.f", claims["user_id"]), 10, 64)
+		if err != nil {
+			return nil, err
+		}
+		return &AccessDetails{
+			AccessUuid: accessUuid,
+			UserId:   userId,
+		}, nil
+	}
+	return nil, err
+}
+
+func verifyToken(r *http.Request) (*jwt.Token, error) {
+	tokenString := extractToken(r)
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		//Make sure that the token method conform to "SigningMethodHMAC"
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(os.Getenv("ACCESS_SECRET")), nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return token, nil
+}
+
+ func extractToken(r *http.Request) string {
+	bearToken := r.Header.Get("Authorization")
+	//normally Authorization the_token_xxx
+	strArr := strings.Split(bearToken, " ")
+	if len(strArr) == 2 {
+		return strArr[1]
+	}
+	return ""
+}
+
+func tokenValid(r *http.Request) error {
+	token, err := verifyToken(r)
+	if err != nil {
+		return err
+	}
+	if _, ok := token.Claims.(jwt.Claims); !ok && !token.Valid {
+		return err
+	}
+	return nil
+}
